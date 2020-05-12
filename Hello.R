c37bf187b9f7722ca11846450f8d00b73448c6af
Kiril Spiridonov <kirilspiridonov@abv.bg>
2020-05-12 10:47:37 +0300

    week 1

--- Hello.R
+++ Hello.R
@@ -1,1 +1,17 @@
-print("Hello")
+colnames(dd)
+tail(dd, 2)
+dd[47,]
+sum(is.na(dd$Ozone))
+bad <- is.na(dd)
+dds <- na.omit(dd)
+mean(dds$Ozone)
+filter <- dds$Ozone > 31
+dds <- dds[filter,]
+filter <- dds$Temp > 90
+dds <- dds[filter,]
+mean(dds$Solar.R)
+dds <- na.omit(dd)
+dds <- dd
+filter <- dds$Month == 5
+dds <- dds[filter,]
+max(dds$Ozone)
